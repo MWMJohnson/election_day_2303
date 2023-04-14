4fc45e8380048a2e40c320691d76f852113dbeee
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-14 10:24:21 -0600

    Feat: add #candidates test for Election spec.

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Election do
   before(:each) do 
     @diana = Candidate.new({name: "Diana D", party: :democrat})
     @texas_race = Race.new("Texas Governor")
+    @ohio_race = Race.new("Ohio Governor")
     @election = Election.new("2024")
   end
 
@@ -26,9 +27,29 @@ RSpec.describe Election do
     it "adds a race to the races list" do
       expect(@election.races).to eq([])
 
-      @election.add_race(@texas_race)
-
+      expect(@election.add_race(@texas_race)).to eq([@texas_race])
       expect(@election.races).to eq([@texas_race])
+
+      expect(@election.add_race(@ohio_race)).to eq([@texas_race, @ohio_race])
+      expect(@election.races).to eq([@texas_race, @ohio_race])
+    end
+  end
+
+  describe "#candidates" do
+    it "shows a list of all candidates in that election year" do
+      expect(@election.candidates).to eq([])
+
+      candidate1 = @texas_race.register_candidate!({name: "Diana D", party: :democrat})
+      candidate2 = @ohio_race.register_candidate!({name: "Roberto R", party: :republican})
+      
+      @election.add_race(@texas_race)
+      expect(@election.candidates).to eq([candidate1])
+
+      @election.add_race(@ohio_race)
+      expect(@election.candidates).to eq([candidate1, candidate2])
+
+      candidate3 = @ohio_race.register_candidate!({name: "Suzy S", party: :independent})
+      expect(@election.candidates).to eq([candidate1, candidate2, candidate3])
     end
   end
 
